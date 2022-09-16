c2fd36003333e5d5b667512fb3c62012a8b1c4ed
Julio Zaldaña <julizaldana@gmail.com>
2022-09-16 00:53:58 -0600

    Cambio en main v2

--- main.py
+++ main.py
@@ -19,10 +19,11 @@ if __name__=='__main__':
             print("Ingresa un nombre a tu pedido:")
             nombrepedido=input()
             print("Bienvenido " + nombrepedido + "!")
+            return menu()
                 
 
         elif respuesta=='2':
             exit()
 
 
-        menu()
+    menu()
