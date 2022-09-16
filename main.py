519177aa69247272f44cd0989217847a5b27e92f
Julio Zaldaña <julizaldana@gmail.com>
2022-09-16 01:03:27 -0600

    Se imprimen datos personales

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ if __name__=='__main__':
         print("---------------------------------------------")
         print("MENU - SHUCOS DE GUATEMALA")
         print("1. Ordenar un shuco")
-        print("2. Salir")
+        print("2. Información Estudiante")
+        print("3. Salir")
         print("---------------------------------------------")
         respuesta = input("Ingrese una opcion: ")
 
@@ -19,10 +20,28 @@ if __name__=='__main__':
             print("Ingresa un nombre a tu pedido:")
             nombrepedido=input()
             print("Bienvenido " + nombrepedido + "!")
+            
+            
+            print("------------------------------------")
+            print("             INGREDIENTES            ")
+            print("Salchicha - 2 minutos")
+            print("Chorizo - 3 minutos")
+            print("Salami - 1.5 minutos")
+            print("Longaniza - 4 minutos")
+            print("Costilla - 6 minutos")
+            print("------------------------------------")
+            print("¿Qué tipo de shuco quieres ordenar?")
+
+            shuco = input("")
+
             return menu()
-                
 
         elif respuesta=='2':
+            print("NOMBRE: Julio Alejandro Zaldaña Ríos")
+            print("CARNET: 202110206")
+            return menu()
+            
+        elif respuesta=='3':
             exit()
 
 
