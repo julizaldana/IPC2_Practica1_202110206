f02413ef0a54919f462f9ceaa4c62d5d570936f8
Julio Zaldaña <julizaldana@gmail.com>
2022-09-16 00:40:05 -0600

    Se modifica clase nodo y funcion clase cola

--- ClaseCola.py
+++ ClaseCola.py
@@ -7,4 +7,21 @@ class Node:
         self.next = None
         self.prev = None
 
+class Cola:
+
+    def __init__(self):
+        self.head = None
+        self.last = None
+
+    #AGREGAR UNA PERSONA A LA COLA
+    def encolar(self, data):
+        if self.last is None:
+            self.head = Node(data)
+            self.last = self.head
+        else:
+            self.last.next = Node(data)
+            self.last.next.prev = self.last
+            self.last = self.last.next
+
+
 
